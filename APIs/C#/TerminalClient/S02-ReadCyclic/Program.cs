be6ce1a668485231f020d972e3f80cc3bd4b838c
Marco Diekmann <M.Diekmann@beckhoff.com>
2021-11-03 13:48:59 +0100

    [TerminalClient] Implemented sample "S02-ReadCyclic".

---
+++ APIs/C#/TerminalClient/S02-ReadCyclic/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using Beckhoff.BA.TerminalClient.Api;
+using Beckhoff.BA.TerminalClient.Tc3;
+
+namespace S02_ReadCyclic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Settings:
+            string sDevNetID = "127.0.0.1.1.1:851";
+            string sObjectPath = "MyPlcProject.MAIN.AnalogValue";
+            Tc3_BA2.BaPlcVariableID bVariableID = Tc3_BA2.BaPlcVariableID.ePresentValue;
+
+            // Initialize:
+            BaApi.BaSite.OnLog += OnLog;
+            BaApi.BaSite.OnReadCyclic += OnCyclicRead;
+
+            // Add device(s) to site:
+            BaApi.BaSite.AddDevice(sDevNetID);
+
+            // Establish site connection:
+            BaApi.BaSite.Connection = true;
+            if (BaApi.BaSite.Connection)
+            {
+                // Find object:
+                BaApi.BaSite.BaBasicObject bObject = BaApi.BaSite.FindObject(sObjectPath);
+                if (bObject == null)
+                    Console.WriteLine(string.Format("Object '{0}' not found!", sObjectPath));
+                else
+                {
+                    // Add job to cyclically read a variable's value:
+                    BaApi.BaSite.Devices[0].AddReadJob_Cyclic(new BaApi.BaSumCommand(bObject, false, (_bPlcVar) => (_bPlcVar.ID == bVariableID)));
+
+                    Console.WriteLine("Press key to exit...");
+                    Console.WriteLine();
+                    Console.ReadKey();
+                }
+
+                // Disconnect from site:
+                BaApi.BaSite.Connection = false;
+                Debug.Assert(!BaApi.BaSite.Connection);
+            }
+        }
+
+
+        #region Events
+        private static void OnLog(BaApi.BaLogType eIcon, string sStackTrace, string sMessage, string sProcess = "", object oResult = null)
+        {
+            Console.WriteLine(sMessage);
+        }
+        private static void OnCyclicRead(BaApi.BaSumCommand sCommand, Object oTag)
+        {
+            // Display all read variable values:
+            foreach (BaApi.BaPlcVariable _bVar in sCommand.Variables)
+                Console.WriteLine(string.Format("Value of variable '{0}': {1}", _bVar.Symbol.InstancePath, _bVar.Value));
+        }
+        #endregion
+    }
+}
