10e426acb8828399a5b7d4f09c2bb3475120f182
Marco Diekmann <M.Diekmann@beckhoff.com>
2021-11-03 13:48:51 +0100

    [TerminalClient] Implemented sample "S01-SimpleRead".

---
+++ APIs/C#/TerminalClient/S01-SimpleRead/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using Beckhoff.BA.TerminalClient.Api;
+using Beckhoff.BA.TerminalClient.Tc3;
+
+namespace S01_ReadValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Settings:
+            string sDevNetID = "127.0.0.1.1.1:851";
+            string sObjectPath = "MyPlcProject.MAIN.AnalogValue";
+            Tc3_BA2.BaPlcVariableID bVariableID = Tc3_BA2.BaPlcVariableID.ePresentValue;
+
+            // Initialize:
+            BaApi.BaSite.OnLog += OnLog;
+
+            // Add device(s) to site:
+            BaApi.BaSite.AddDevice(sDevNetID);
+
+            // Establish site connection:
+            BaApi.BaSite.Connection = true;
+            if (BaApi.BaSite.Connection)
+            {
+                // Find object:
+                BaApi.BaSite.BaBasicObject bObject = BaApi.BaSite.FindObject(sObjectPath);
+                if (bObject == null)
+                    Console.WriteLine(string.Format("Object '{0}' not found!", sObjectPath));
+                else
+                {
+                    BaApi.BaPlcVariable bVariable;
+
+                    // Get variable from object and read current value:
+                    if (!bObject.PlcVariables.TryGetValue(bVariableID, out bVariable))
+                        Console.WriteLine(string.Format("Variable '{0}~{1}' not found!", sObjectPath, bVariableID));
+                    else
+                    {
+                        // Read variable value:
+                        bool _bRepeat;
+                        do
+                        {
+                            _bRepeat = !bVariable.ReadValue();
+                            if (_bRepeat)
+                                Console.WriteLine(string.Format("Failed to read variable '{0}~{1}'!", sObjectPath, bVariableID));
+                            else
+                            {
+                                Console.WriteLine(string.Format("Value of variable '{0}~{1}': {2}", sObjectPath, bVariableID, bVariable.Value));
+
+                                Console.WriteLine("Press [enter] to read again...");
+                                _bRepeat = (Console.ReadKey().Key == ConsoleKey.Enter);
+                            }
+
+                        } while (_bRepeat);
+                    }
+                }
+            }
+
+            // Disconnect from site:
+            BaApi.BaSite.Connection = false;
+            Debug.Assert(!BaApi.BaSite.Connection);
+
+            Console.WriteLine();
+            Console.WriteLine("Press key to exit...");
+            Console.ReadKey();
+        }
+
+
+        #region Events
+        private static void OnLog(BaApi.BaLogType eIcon, string sStackTrace, string sMessage, string sProcess = "", object oResult = null)
+        {
+            Console.WriteLine(sMessage);
+        }
+        #endregion
+    }
+}
